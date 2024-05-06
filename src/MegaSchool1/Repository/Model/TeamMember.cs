52cc58d67e34bafca34b52433929ec017f8d4e27
Jerome Bell <jeromebell@gmail.com>
2024-05-06 10:53:25 -0500

    team member add, edit, delete

---
+++ src/MegaSchool1/Repository/Model/TeamMember.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MegaSchool1.Repository.Model;
+
+public class TeamMember
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = default!;
+
+    [JsonPropertyName("member_id")]
+    public string MemberId { get; set; } = default!;
+}
