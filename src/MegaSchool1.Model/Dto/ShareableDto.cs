f8450f6173af3a0c3e50d7765b07d4a3c899f249
Jerome Bell <jeromebell@gmail.com>
2025-09-17 15:30:42 -0500

    done

--- src/MegaSchool1.Model/Dto/ShareableDto.cs
+++ src/MegaSchool1.Model/Dto/ShareableDto.cs
@@ -3,106 +3,117 @@ using System.Text.Json.Serialization;
 
 namespace MegaSchool1.Model.Dto;
 
-public readonly record struct ShareableDto(
-    [property: JsonPropertyName("contentId"), ConfigurationKeyName("contentId")]
-    Content ContentId,
-    
-    [property: JsonPropertyName("videoId"), ConfigurationKeyName("videoId")]
-    string? Id,
-    
-    [property: JsonPropertyName("platform"), ConfigurationKeyName("platform")]
-    VideoPlatform Platform,
-    
-    [property: JsonPropertyName("videoHash"), ConfigurationKeyName("videoHash")]
-    string? Hash,
-
-    [property: JsonPropertyName("userHandle"), ConfigurationKeyName("userHandle")]
-    string? UserHandle,
-  
-    [property: JsonPropertyName("url"), ConfigurationKeyName("url")]
-    string? Url,
-    
-    [property: JsonPropertyName("appTitle"), ConfigurationKeyName("appTitle")]
-    string AppTitle,
-    
-    [property: JsonPropertyName("shareableTitle"), ConfigurationKeyName("shareableTitle")]
-    string ShareableTitle,
-    
-    [property: JsonPropertyName("duration"), ConfigurationKeyName("duration")]
-    TimeSpan? Duration,
-    
-    [property: JsonPropertyName("imageId"), ConfigurationKeyName("imageId")]
-    Image? Image,
-    
-    [property: JsonPropertyName("imageUrl"), ConfigurationKeyName("imageUrl")]
-    string? ImageUrl,
-    
-    [property: JsonPropertyName("capturePageImageId"), ConfigurationKeyName("capturePageImageId")]
-    Image? CapturePageImage,
-    
-    [property: JsonPropertyName("capturePageImageUrl"), ConfigurationKeyName("capturePageImageUrl")]
-    string? CapturePageImageUrl,
-    
-    [property: JsonPropertyName("downloadText"), ConfigurationKeyName("downloadText")]
-    string? DownloadText,
-    
-    [property: JsonPropertyName("downloadUrl"), ConfigurationKeyName("downloadUrl")]
-    string? DownloadUrl,
-    
-    [property: JsonPropertyName("promoExpiration"), ConfigurationKeyName("promoExpiration")]
-    DateTimeOffset? PromoExpiration,
-    
-    [property: JsonPropertyName("event"), ConfigurationKeyName("event")]
-    EventDto? Event,
-    
-    [property: JsonPropertyName("showBusinessSignUp"), ConfigurationKeyName("showBusinessSignUp")]
-    bool ShowBusinessSignUp,
-    
-    [property: JsonPropertyName("hideShortCodePrompt"), ConfigurationKeyName("hideShortCodePrompt")]
-    bool HideShortCodePrompt,
-    
-    [property: JsonPropertyName("images"), ConfigurationKeyName("images")]
-    ImageInfo[]? Images,
-    
-    [property: JsonPropertyName("auxText"), ConfigurationKeyName("auxText")]
-    string? AuxiliaryText,
-    
-    [property: JsonPropertyName("auxTexts"), ConfigurationKeyName("auxTexts")]
-    string[]? AuxiliaryTexts,
-    
-    [property: JsonPropertyName("alternateVideos"), ConfigurationKeyName("alternateVideos")]
-    VideoDto[]? AlternateVideos,
-    
-    [property: JsonPropertyName("metadata"), ConfigurationKeyName("metadata")]
-    string? Metadata,
-    
-    [property: JsonPropertyName("flyerImageUrl"), ConfigurationKeyName("flyerImageUrl")]
-    string? FlyerImageUrl,
-    
-    [property: JsonPropertyName("className"), ConfigurationKeyName("className")]
-    string? ClassName,
-    
-    [property: JsonPropertyName("classDescription"), ConfigurationKeyName("classDescription")]
-    string? ClassDescription,
-    
-    [property: JsonPropertyName("start"), ConfigurationKeyName("start")]
-    TimeSpan? Start,
-    
-    [property: JsonPropertyName("hideCapturePageVideo"), ConfigurationKeyName("hideCapturePageVideo")]
-    bool HideCapturePageVideo,
-    
-    [property: JsonPropertyName("clipId"), ConfigurationKeyName("clipId")]
-    string? ClipId,
-    
-    [property: JsonPropertyName("clipTimestamp"), ConfigurationKeyName("clipTimestamp")]
-    string? ClipTimestamp,
-    
-    [property: JsonPropertyName("showHeaderImage"), ConfigurationKeyName("showHeaderImage")]
-    bool ShowHeaderImage = true,
-
-    [property: JsonPropertyName("promo"), ConfigurationKeyName("promo")]
-    string? Promo  = "For $100 off, text"
-)
+public record ShareableDto
 {
+    [JsonPropertyName("contentId")]
+    [ConfigurationKeyName("contentId")]
+    public Content ContentId { get; init; }
+
+    [JsonPropertyName("videoId")]
+    [ConfigurationKeyName("videoId")]
+    public string? Id { get; init; }
+
+    [JsonPropertyName("platform")]
+    public VideoPlatform Platform { get; init; }
+
+    [JsonPropertyName("videoHash")]
+    [ConfigurationKeyName("videoHash")]
+    public string? Hash { get; init; }
+
+    [JsonPropertyName("userHandle")]
+    public string? UserHandle { get; init; }
+
+    [JsonPropertyName("url")]
+    public string? Url { get; init; }
+
+    [JsonPropertyName("appTitle")]
+    public string AppTitle { get; init; } = default!;
+
+    [JsonPropertyName("shareableTitle")]
+    public string ShareableTitle { get; init; } = default!;
+    
+    [JsonPropertyName("duration")]
+    public TimeSpan? Duration { get;  init; }
+
+    [JsonPropertyName("imageId")]
+    [ConfigurationKeyName("imageId")]
+    public Image? Image { get; init; }
+
+    [JsonPropertyName("imageUrl")]
+    public string? ImageUrl { get; init; }
+    
+    [JsonPropertyName("showHeaderImage")]
+    public bool ShowHeaderImage { get; init; } = true;
+
+    [JsonPropertyName("capturePageImageId")]
+    [ConfigurationKeyName("capturePageImageId")]
+    public Image? CapturePageImage { get; init; }
+
+    [JsonPropertyName("capturePageImageUrl")]
+    [ConfigurationKeyName("capturePageImageUrl")]
+    public string? CapturePageImageUrl { get; init; }
+    
+    [JsonPropertyName("downloadText")]
+    public string? DownloadText { get; init; }
+
+    [JsonPropertyName("downloadUrl")]
+    public string? DownloadUrl { get; init; }
+
+    [JsonPropertyName("promo")]
+    public string? Promo { get; init; } = "For $100 off, text";
+
+    [JsonPropertyName("promoExpiration")]
+    public DateTimeOffset? PromoExpiration { get; init; }
+
+    [JsonPropertyName("event")]
+    public EventDto? Event { get; init; }
+
+    [JsonPropertyName("showBusinessSignUp")]
+    public bool ShowBusinessSignUp { get; init; }
+
+    [JsonPropertyName("hideShortCodePrompt")]
+    public bool HideShortCodePrompt { get; init; }
+
+    [JsonPropertyName("images")]
+    public ImageInfo[] Images { get; init; } = [];
+
+    [JsonPropertyName("auxText")]
+    [ConfigurationKeyName("auxText")]
+    public string? AuxiliaryText { get; init; }
+    
+    [JsonPropertyName("auxTexts")]
+    [ConfigurationKeyName("auxTexts")]
+    public string[] AuxiliaryTexts { get; init; } = [];
+
+    [JsonPropertyName("alternateVideos")]
+    public VideoDto[] AlternateVideos { get; init; } = [];
+
+    [JsonPropertyName("metadata")]
+    public string? Metadata { get; init; }
+
+    [JsonPropertyName("flyerImageUrl")]
+    public string? FlyerImageUrl { get; init; }
+
+    [JsonPropertyName("className")]
+    public string? ClassName { get; init; }
+
+    [JsonPropertyName("classDescription")]
+    public string? ClassDescription { get; init; }
+
+    [JsonPropertyName("start")]
+    public TimeSpan? Start { get;  init; }
+    
+    [JsonPropertyName("hideCapturePageVideo")]
+    [ConfigurationKeyName("hideCapturePageVideo")]
+    public bool HideCapturePageVideo { get;  init; }
+    
+    [JsonPropertyName("clipId")]
+    [ConfigurationKeyName("clipId")]
+    public string? ClipId { get; init; }
+    
+    [JsonPropertyName("clipTimestamp")]
+    [ConfigurationKeyName("clipTimestamp")]
+    public string? ClipTimestamp { get; init; }
+    
     public override string ToString() => $"{ContentId}";
 }
